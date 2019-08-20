88327685739225bc849abca9fcd8771f2401c5f7
Jose2432 <54084178+Jose2432@users.noreply.github.com>
2019-08-20 10:52:11 -0500

    Add files via upload

---
+++ practica02/Sintaxis.java
@@ -0,0 +1,27 @@
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+	public static void main(String[] args) {
+		int a, b = 20, c;
+		a = 3; c = -7;
+
+		boolean prop = true;
+		short ochoBits = 128;
+		int dieciseisBits = -32769;
+
+		double discriminante = b * b - 4 * a * c;
+		double x1 = (-b + sqrt(discriminante)) / (2 * a);
+		double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+		char ch1, ch2;
+		ch1 = 'g'; ch2 = 'w';
+		int d = 1 / (a+1 - 3);
+
+		// Declaraciones
+    long var_1 = 2147483648L;
+    String quote = "'";
+
+	}
+
+}
